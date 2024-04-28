8ae7da87a5a6d76e71f66810f3d91ec48c54c964
matchaejayyy <145851990+matchaejayyy@users.noreply.github.com>
2024-04-29 00:52:33 +0800

    Update forgot_password.dart

--- flutter_application_1/lib/pages/forgot_password.dart
+++ flutter_application_1/lib/pages/forgot_password.dart
@@ -1,13 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+
 class ForgotPasswordScreen extends StatefulWidget {
+  @override
   _ForgotPasswordScreenState createState() => _ForgotPasswordScreenState();
 }
 
 class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
-  final TextEditingController _emailController = TextEditingController();
+  final _emailController = TextEditingController();
 
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
 
+  Future<void> passwordReset() async {
+    try {
+      await FirebaseAuth.instance
+          .sendPasswordResetEmail(email: _emailController.text.trim());
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('A password reset link has been sent to your email.'),
+        ),
+      );
+    } on FirebaseAuthException catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Invalid Email"), // Display the error message
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,50 +42,49 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const SizedBox(height:200),
-                const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 50),
-                child: Row(
+            const SizedBox(height: 200),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 50),
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                  'FORGOT PASSWORD',
+                    'FORGOT PASSWORD',
                     style: TextStyle(
-                      color: Colors.black, 
-                      fontWeight: FontWeight.bold, fontSize: 30),
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
                   ),
                 ],
-                ),
               ),
-
-          
-            const SizedBox(height: 8),
-             Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(hintText: 'Enter your email'),
-              keyboardType: TextInputType.emailAddress,
-              )
             ),
-            
-            const SizedBox(height: 10),
+            const SizedBox(height: 8),
             Padding(
               padding: const EdgeInsets.all(20.0),
-              child: ElevatedButton(
-              onPressed: () {
-                // TODO: Implement password reset logic here
-                final email = _emailController.text;
-                print('Reset password for $email');
-              },
-              child: const Text('Submit'),
-              )
+              child: TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(hintText: 'Enter your email'),
+                keyboardType: TextInputType.emailAddress,
+              ),
             ),
+            const SizedBox(height: 5),
+            
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: MaterialButton(
+                onPressed: passwordReset, // Call the function directly on press
+                child: const Text("Reset Password"),
+                color: Colors.red,
+              ),
+            ),
+            
+            
           ],
         ),
       ),
     );
   }
 }
-                
+
   
