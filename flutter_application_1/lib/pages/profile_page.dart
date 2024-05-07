49fc3d3ed687aae427d122f64361e4489733ed66
matchaejayyy <145851990+matchaejayyy@users.noreply.github.com>
2024-05-07 15:55:33 +0800

    Update profile_page.dart

--- flutter_application_1/lib/pages/profile_page.dart
+++ flutter_application_1/lib/pages/profile_page.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/LoginPage.dart';
 import 'package:flutter_application_1/pages/activity_page.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
-import 'package:flutter_application_1/pages/login_or_register_page.dart';
 import 'package:flutter_application_1/pages/map_page.dart';
 import 'package:flutter_application_1/pages/splash_screen.dart';
 
